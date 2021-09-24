228666594095fdae986b4c929f3e0476c5f43984
ardaerbaharli <ardaerbaharli1@gmail.com>
2021-09-24 11:05:24 +0300

    log file

---
+++ chastocaBot/Log.cs
@@ -0,0 +1,10 @@
+﻿
+namespace chastocaBot
+{
+    class Log
+    {
+        public string Message { get; set; }
+        public string Sender { get; set; }
+        public string LogName { get; set; }
+    }
+}
